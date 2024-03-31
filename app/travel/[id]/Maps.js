5fd654f7b611a83d15d7f341933f88238c8320b8
SaChan <vltpcks@gmail.com>
2024-03-31 22:10:39 +0900

    search map

--- app/travel/[id]/Maps.js
+++ app/travel/[id]/Maps.js
@@ -4,36 +4,44 @@ import useGoogle from "react-google-autocomplete/lib/usePlacesAutocompleteServic
 
 export default function Maps() {
   const [map, setMap] = useState(null);
-  const [place, setPlace] = useState("");
+  const [place, setPlace] = useState("ChIJzzlcLQGifDURm_JbQKHsEX4");
+  const [lat, setLat] = useState(37.572389);
+  const [lng, setLng] = useState(126.9769117);
   const { placePredictions, getPlacePredictions, isPlacePredictionsLoading } =
     useGoogle({
       apiKey: process.env.NEXT_PUBLIC_API,
     });
   const ref = useRef();
   useEffect(() => {
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ placeId: place }, (result) => {
+      setLat(result[0].geometry.location.lat());
+      setLng(result[0].geometry.location.lng());
+    });
     const newMap = new google.maps.Map(ref.current, {
-      center: { lat: 37.569227, lng: 126.9777256 },
+      center: { lat: lat, lng: lng },
       zoom: 16,
     });
+    console.log(lat, lng, place);
 
     setMap(newMap);
-  }, []);
-
+  }, [place]);
+  // console.log(map);
   return (
     <div>
       <input
-        value={place}
         onChange={(e) => {
           getPlacePredictions({ input: e.target.value });
           // setChosen(e.target.value);
-          setPlace(e.target.value);
         }}
       />
       {placePredictions.length != 0
-        ? placePredictions.map((item) => (
+        ? placePredictions.map((item, idx) => (
             <h1
+              key={idx}
               onClick={(e) => {
-                console.log(item);
+                setPlace(item.place_id);
+                console.log(item.place_id, item);
               }}
             >
               {item.description}
