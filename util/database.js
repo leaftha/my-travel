95567b3ce32dc8bbf3c1518faf9d6cd160193a95
leaftha <vltpcks@gmail.com>
2024-04-04 17:07:37 +0900

    mongoose error fix

--- util/database.js
+++ util/database.js
@@ -1,15 +1,14 @@
-import { MongoClient } from "mongodb";
-const url =
-  "mongodb+srv://vltpcks:coffee115@cluster0.sbh6f5y.mongodb.net/?retryWrites=true&w=majority";
-const options = { useNewUrlParser: true };
+import { MongoClient } from 'mongodb';
+const url = 'mongodb+srv://vltpcks:coffee115@cluster0.sbh6f5y.mongodb.net/?retryWrites=true&w=majority';
+const options = {};
 let connectDB;
 
-if (process.env.NODE_ENV === "development") {
-  if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect();
-  }
-  connectDB = global._mongo;
+if (process.env.NODE_ENV === 'development') {
+    if (!global._mongo) {
+        global._mongo = new MongoClient(url, options).connect();
+    }
+    connectDB = global._mongo;
 } else {
-  connectDB = new MongoClient(url, options).connect();
+    connectDB = new MongoClient(url, options).connect();
 }
 export { connectDB };
