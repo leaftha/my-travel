dfba888afe4dcfc71338f373df992faeab0f0201
SaChan <vltpcks@gmail.com>
2024-04-06 12:46:00 +0900

    delete to did

--- app/travel/[id]/toDid.js
+++ app/travel/[id]/toDid.js
@@ -1,50 +1,61 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function ToDId({ day, id }) {
-    const [content, setContent] = useState('');
-    const [contents, setContents] = useState([...day.content]);
-    return (
-        <div>
-            <h1>To Did</h1>
-            <input
-                value={content}
-                onChange={(e) => {
-                    setContent(e.target.value);
-                }}
-            />
-            <button
-                onClick={() => {
-                    fetch('/api/post/addToDid', {
-                        method: 'POST',
-                        body: JSON.stringify({ id: id, day: day.day, content: content }),
-                    });
-                    setContents([content, ...contents]);
-                    setContent('');
-                }}
+  const [content, setContent] = useState("");
+  const [contents, setContents] = useState([...day.content]);
+
+  function deletContent(idx) {
+    setContents(contents.filter((item, index) => index != idx));
+  }
+
+  return (
+    <div>
+      <h1>To Did</h1>
+      <input
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          fetch("/api/post/addToDid", {
+            method: "POST",
+            body: JSON.stringify({ id: id, day: day.day, content: content }),
+          });
+          setContents([content, ...contents]);
+          setContent("");
+        }}
+      >
+        입력
+      </button>
+      {contents.length === 0 ? (
+        <h1>했던일을 입력하세요</h1>
+      ) : (
+        contents.map((item, idx) => (
+          <div key={idx}>
+            <h1>{item}</h1>
+            <h1
+              onClick={() => {
+                fetch("/api/post/deleteToDid", {
+                  method: "POST",
+                  body: JSON.stringify({
+                    id: id,
+                    day: day.day,
+                    contents: [...contents],
+                    idx: idx,
+                  }),
+                });
+                deletContent(idx);
+              }}
             >
-                입력
-            </button>
-            {contents.length === 0 ? (
-                <h1>했던일을 입력하세요</h1>
-            ) : (
-                contents.map((item, idx) => (
-                    <div key={idx}>
-                        <h1>{item}</h1>
-                        <h1
-                            onClick={() => {
-                                fetch('/api/post/deleteToDid', {
-                                    method: 'POST',
-                                    body: JSON.stringify({ contents: [...contents], idx: idx }),
-                                });
-                            }}
-                        >
-                            X
-                        </h1>
-                    </div>
-                ))
-            )}
-        </div>
-    );
+              X
+            </h1>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
--- pages/api/post/deleteToDid.js
+++ pages/api/post/deleteToDid.js
@@ -1,17 +1,26 @@
-import { connectDB } from '@/util/database';
-import { ObjectId } from 'mongodb';
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        req.body = JSON.parse(req.body);
-        const ThisDay = req.body.day - 1;
-        let db = (await connectDB).db('travel');
-        let obj = await db.collection('travelPost').findOne({ _id: new ObjectId(req.body.id) });
-        let newDay = obj.days[ThisDay];
-        let content = newDay.content;
-        let newContent = [req.body.content, ...content];
-        newDay.content = newContent;
-        await db
-            .collection('travelPost')
-            .updateOne({ _id: new ObjectId(req.body.id) }, { $set: { [`days.${ThisDay}`]: newDay } });
+  if (req.method === "POST") {
+    req.body = JSON.parse(req.body);
+    const ThisDay = req.body.day - 1;
+    let db = (await connectDB).db("travel");
+    let obj = await db
+      .collection("travelPost")
+      .findOne({ _id: new ObjectId(req.body.id) });
+    let newDay = obj.days[ThisDay];
+    let contents = newDay.content;
+    let newContent = [];
+    for (let i = 0; i < contents.length; i++) {
+      if (i === req.body.idx) continue;
+      newContent.push(contents[i]);
     }
+    newDay.content = newContent;
+    await db
+      .collection("travelPost")
+      .updateOne(
+        { _id: new ObjectId(req.body.id) },
+        { $set: { [`days.${ThisDay}`]: newDay } }
+      );
+  }
 }
