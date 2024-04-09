d7a7b0cea55feb61b2f7a16cab73e748230b5040
leaftha <vltpcks@gmail.com>
2024-04-09 11:58:17 +0900

    longin btn and logout btn session

--- app/loginbtn.js
+++ app/loginbtn.js
@@ -1,24 +1,27 @@
-"use client";
-import { signIn } from "next-auth/react";
-import { signOut } from "next-auth/react";
+'use client';
+import { signIn } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 
-export default function LoginBtn() {
-  return (
-    <div>
-      <button
-        onClick={() => {
-          signIn();
-        }}
-      >
-        로그인
-      </button>
-      <button
-        onClick={() => {
-          signOut();
-        }}
-      >
-        로그아웃
-      </button>
-    </div>
-  );
+export default function LoginBtn({ session }) {
+    return (
+        <div>
+            {session ? (
+                <button
+                    onClick={() => {
+                        signOut();
+                    }}
+                >
+                    로그아웃
+                </button>
+            ) : (
+                <button
+                    onClick={() => {
+                        signIn();
+                    }}
+                >
+                    로그인
+                </button>
+            )}
+        </div>
+    );
 }
--- app/page.js
+++ app/page.js
@@ -1,27 +1,27 @@
-import Link from "next/link";
-import LoginBtn from "./loginbtn";
-import { authOptions } from "@/pages/api/auth/[...nextauth].js";
-import { getServerSession } from "next-auth";
-import NewTravel from "./newTravel";
-import TravelList from "./travelList";
+import Link from 'next/link';
+import LoginBtn from './loginbtn';
+import { authOptions } from '@/pages/api/auth/[...nextauth].js';
+import { getServerSession } from 'next-auth';
+import NewTravel from './newTravel';
+import TravelList from './travelList';
 
 export default async function Home() {
-  let session = await getServerSession(authOptions);
+    let session = await getServerSession(authOptions);
 
-  return (
-    <div>
-      {session ? <div>로그인</div> : <div>로그인 X</div>}
-      <LoginBtn />
-      {session ? (
-        ""
-      ) : (
-        <Link className="register" href="register">
-          회원가입
-        </Link>
-      )}
+    return (
+        <div>
+            {session ? <div>로그인</div> : <div>로그인 X</div>}
+            <LoginBtn session={session} />
+            {session ? (
+                ''
+            ) : (
+                <Link className="register" href="register">
+                    회원가입
+                </Link>
+            )}
 
-      {session ? <NewTravel user={session} /> : ""}
-      {session ? <TravelList user={session} /> : ""}
-    </div>
-  );
+            {session ? <NewTravel user={session} /> : ''}
+            {session ? <TravelList user={session} /> : ''}
+        </div>
+    );
 }
