b2814cd48a16547cb6916867d7ad28a3dcf0cbf2
leaftha <vltpcks@gmail.com>
2024-04-25 12:24:57 +0900

    비공개 공개 버튼 삭제

--- app/travelDetail/[id]/likes.js
+++ app/travelDetail/[id]/likes.js
@@ -1,69 +1,44 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-export default function Likse({ like, email, id, user, isprivate }) {
-  // 유저의 좋아요한 게시물 아이디
-  const [likes, setLikes] = useState([...user.likes]);
-  // 게시물의 비공개 여부
-  const [isPrivate, setIsPrivate] = useState(isprivate);
-  const [privateString, sePrivateString] = useState(
-    isPrivate ? "비공개" : "공개"
-  );
-  //현 게시물의 좋아요 갯수
-  const [likeNum, setlikeNum] = useState(like);
-  return (
-    <div>
-      <h1>{likeNum}</h1>
-      <button
-        onClick={() => {
-          if (likes.indexOf(id) === -1) {
-            // 유저가 좋아요 하지 않을 경우
-            fetch("/api/post/upLikes", {
-              method: "POST",
-              body: JSON.stringify({
-                email: email,
-                id: id,
-              }),
-            });
-            setlikeNum(likeNum + 1);
-            setLikes([...likes, id]);
-          } else {
-            // 유저가 좋아요 했을경우
-            fetch("/api/post/deleteLike", {
-              method: "POST",
-              body: JSON.stringify({
-                email: email,
-                id: id,
-              }),
-            });
-            setLikes(likes.filter((item, index) => item != id));
-            setlikeNum(likeNum - 1);
-          }
-        }}
-      >
-        좋아요
-      </button>
-      <button
-        onClick={() => {
-          fetch("/api/post/changePrivate", {
-            method: "POST",
-            body: JSON.stringify({
-              email: email,
-              id: id,
-              private: !isPrivate,
-            }),
-          });
-          setIsPrivate(!isPrivate);
-          if (privateString === "비공개") {
-            sePrivateString("공개");
-          } else {
-            sePrivateString("비공개");
-          }
-        }}
-      >
-        {privateString}
-      </button>
-    </div>
-  );
+export default function Likse({ like, email, id, user }) {
+    // 유저의 좋아요한 게시물 아이디
+    const [likes, setLikes] = useState([...user.likes]);
+    //현 게시물의 좋아요 갯수
+    const [likeNum, setlikeNum] = useState(like);
+    return (
+        <div>
+            <h1>{likeNum}</h1>
+            <button
+                onClick={() => {
+                    if (likes.indexOf(id) === -1) {
+                        // 유저가 좋아요 하지 않을 경우
+                        fetch('/api/post/upLikes', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                email: email,
+                                id: id,
+                            }),
+                        });
+                        setlikeNum(likeNum + 1);
+                        setLikes([...likes, id]);
+                    } else {
+                        // 유저가 좋아요 했을경우
+                        fetch('/api/post/deleteLike', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                email: email,
+                                id: id,
+                            }),
+                        });
+                        setLikes(likes.filter((item, index) => item != id));
+                        setlikeNum(likeNum - 1);
+                    }
+                }}
+            >
+                좋아요
+            </button>
+        </div>
+    );
 }
--- app/travelDetail/[id]/page.js
+++ app/travelDetail/[id]/page.js
@@ -8,11 +8,11 @@ import Link from 'next/link';
 import Likse from './likes';
 
 export default async function Detail(props) {
-    // let session = await getServerSession(authOptions);
+    let session = await getServerSession(authOptions);
     let db = (await connectDB).db('travel');
     let result = await db.collection('travelPost').findOne({ _id: new ObjectId(props.params.id) });
-    // let user = await db.collection('user_id').findOne({ email: session.user.email });
-    // user._id = user._id.toString();
+    let user = await db.collection('user_id').findOne({ email: session.user.email });
+    user._id = user._id.toString();
     let last_day = 0;
     if (result.days.length != 0) {
         last_day = result.days.length;
@@ -21,13 +21,7 @@ export default async function Detail(props) {
     return (
         <div>
             <Link href="/travelList">리스트</Link>
-            {/* <Likse
-                isprivate={result.private}
-                like={result.like}
-                user={user}
-                id={props.params.id}
-                email={session.user.email}
-            /> */}
+            <Likse like={result.like} user={user} id={props.params.id} email={session.user.email} />
             <h1>{result.title}</h1>
             {result.days.length === 0 ? (
                 <h1>스케줄을 추가 하세요</h1>
