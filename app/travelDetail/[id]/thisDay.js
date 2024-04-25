b88b99907ac29590226d0f58529d436742fd1d8c
leaftha <vltpcks@gmail.com>
2024-04-25 12:14:53 +0900

    여행 상세 페이지 생성

---
+++ app/travelDetail/[id]/thisDay.js
@@ -0,0 +1,44 @@
+"use client";
+
+import { Wrapper } from "@googlemaps/react-wrapper";
+import Maps from "./Maps";
+import { useState } from "react";
+
+export default function ThisDay({ day, id }) {
+  const [money, setMoney] = useState(day.money);
+  return (
+    <div>
+      <h1>{day.day}</h1>
+
+      <label>지출</label>
+      <input
+        type="number"
+        onChange={(e) => {
+          setMoney(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          fetch("/api/post/addDateMoney", {
+            method: "POST",
+            body: JSON.stringify({
+              id: id,
+              day: day.day,
+              money: money,
+            }),
+          });
+        }}
+      >
+        입력
+      </button>
+      <h1>{money}</h1>
+
+      <Wrapper
+        apiKey={process.env.NEXT_PUBLIC_API}
+        libraries={["places", "marker"]}
+      >
+        <Maps id={id} day={day} />
+      </Wrapper>
+    </div>
+  );
+}
