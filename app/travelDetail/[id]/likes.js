db62bca69fdcd5d935c495485b810578861bb09e
leaftha <vltpcks@gmail.com>
2024-04-29 13:28:37 +0900

    로그아웃 상태인 유저의 좋아요 기능 제거

--- app/travelDetail/[id]/likes.js
+++ app/travelDetail/[id]/likes.js
@@ -2,43 +2,47 @@
 
 import { useEffect, useState } from 'react';
 
-export default function Likse({ like, email, id, user }) {
+export default function Likse({ like, id, user }) {
     // 유저의 좋아요한 게시물 아이디
-    const [likes, setLikes] = useState([...user.likes]);
+    const [likes, setLikes] = useState(user != null ? [...user.likes] : null);
     //현 게시물의 좋아요 갯수
     const [likeNum, setlikeNum] = useState(like);
     return (
         <div>
             <h1>{likeNum}</h1>
-            <button
-                onClick={() => {
-                    if (likes.indexOf(id) === -1) {
-                        // 유저가 좋아요 하지 않을 경우
-                        fetch('/api/post/upLikes', {
-                            method: 'POST',
-                            body: JSON.stringify({
-                                email: email,
-                                id: id,
-                            }),
-                        });
-                        setlikeNum(likeNum + 1);
-                        setLikes([...likes, id]);
-                    } else {
-                        // 유저가 좋아요 했을경우
-                        fetch('/api/post/deleteLike', {
-                            method: 'POST',
-                            body: JSON.stringify({
-                                email: email,
-                                id: id,
-                            }),
-                        });
-                        setLikes(likes.filter((item, index) => item != id));
-                        setlikeNum(likeNum - 1);
-                    }
-                }}
-            >
-                좋아요
-            </button>
+            {user != null ? (
+                <button
+                    onClick={() => {
+                        if (likes.indexOf(id) === -1) {
+                            // 유저가 좋아요 하지 않을 경우
+                            fetch('/api/post/upLikes', {
+                                method: 'POST',
+                                body: JSON.stringify({
+                                    email: user.email,
+                                    id: id,
+                                }),
+                            });
+                            setlikeNum(likeNum + 1);
+                            setLikes([...likes, id]);
+                        } else {
+                            // 유저가 좋아요 했을경우
+                            fetch('/api/post/deleteLike', {
+                                method: 'POST',
+                                body: JSON.stringify({
+                                    email: user.email,
+                                    id: id,
+                                }),
+                            });
+                            setLikes(likes.filter((item, index) => item != id));
+                            setlikeNum(likeNum - 1);
+                        }
+                    }}
+                >
+                    좋아요
+                </button>
+            ) : (
+                ''
+            )}
         </div>
     );
 }
--- app/travelDetail/[id]/page.js
+++ app/travelDetail/[id]/page.js
@@ -1,44 +1,38 @@
-import { ObjectId } from "mongodb";
-import { connectDB } from "@/util/database.js";
-import { authOptions } from "@/pages/api/auth/[...nextauth].js";
-import { getServerSession } from "next-auth";
+import { ObjectId } from 'mongodb';
+import { connectDB } from '@/util/database.js';
+import { authOptions } from '@/pages/api/auth/[...nextauth].js';
+import { getServerSession } from 'next-auth';
 
-import ThisDay from "./thisDay";
-import Link from "next/link";
-import Likse from "./likes";
+import ThisDay from './thisDay';
+import Link from 'next/link';
+import Likse from './likes';
 
 export default async function Detail(props) {
-  let session = await getServerSession(authOptions);
-  let db = (await connectDB).db("travel");
-  let result = await db
-    .collection("travelPost")
-    .findOne({ _id: new ObjectId(props.params.id) });
-  let user = await db
-    .collection("user_id")
-    .findOne({ email: session.user.email });
-  user._id = user._id.toString();
-  let last_day = 0;
-  if (result.days.length != 0) {
-    last_day = result.days.length;
-  }
+    let session = await getServerSession(authOptions);
+    let db = (await connectDB).db('travel');
 
-  return (
-    <div>
-      <Link href="/travelList">리스트</Link>
-      <Likse
-        like={result.like}
-        user={user}
-        id={props.params.id}
-        email={session.user.email}
-      />
-      <h1>{result.title}</h1>
-      {result.days.length === 0 ? (
-        <h1></h1>
-      ) : (
-        result.days.map((item, idx) => (
-          <ThisDay key={idx} id={props.params.id} day={item} />
-        ))
-      )}
-    </div>
-  );
+    // db에 저장된 현재 id에 해당하는 여행 데이터 검색
+    let result = await db.collection('travelPost').findOne({ _id: new ObjectId(props.params.id) });
+
+    console.log(session);
+
+    // db에 저장된 유저 데이터 검색
+    let user = null;
+    if (session != null) {
+        user = await db.collection('user_id').findOne({ email: session.user.email });
+        user._id = user._id.toString();
+    }
+
+    return (
+        <div>
+            <Link href="/travelList">리스트</Link>
+            <Likse like={result.like} user={user} id={props.params.id} />
+            <h1>{result.title}</h1>
+            {result.days.length === 0 ? (
+                <h1></h1>
+            ) : (
+                result.days.map((item, idx) => <ThisDay key={idx} id={props.params.id} day={item} />)
+            )}
+        </div>
+    );
 }
