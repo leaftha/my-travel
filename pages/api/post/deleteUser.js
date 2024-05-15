af348ab2bf79dc7f1699b3eeef88f05455555f03
SaChan <vltpcks@gmail.com>
2024-05-15 22:29:43 +0900

    회원 탈퇴시 게시물을 같이 삭제

--- pages/api/post/deleteUser.js
+++ pages/api/post/deleteUser.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     let db = (await connectDB).db("travel");
     await db.collection("user_id").deleteOne({ email: req.body.email });
+    await db.collection("travelPost").deleteMany({ email: req.body.email });
   }
   res.status(200).redirect(302, "/");
 }
