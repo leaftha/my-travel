edd713c17a5e724265b2f64ade5e16bf52535e3d
leaftha <vltpcks@gmail.com>
2024-05-17 15:08:49 +0900

    navbar 컴포넌트 위치 변경

--- app/page.js
+++ app/page.js
@@ -1,20 +1,19 @@
-import Link from "next/link";
-import LoginBtn from "./loginbtn";
-import { authOptions } from "@/pages/api/auth/[...nextauth].js";
-import { getServerSession } from "next-auth";
-import NewTravel from "./newTravel";
-import TravelList from "./travelList";
-import Navbar from "./navbar";
+import Link from 'next/link';
+import LoginBtn from './loginbtn';
+import { authOptions } from '@/pages/api/auth/[...nextauth].js';
+import { getServerSession } from 'next-auth';
+import NewTravel from './newTravel';
+import TravelList from './travelList';
+import style from './page.module.css';
 
 export default async function Home() {
-  let session = await getServerSession(authOptions);
+    let session = await getServerSession(authOptions);
 
-  return (
-    <div>
-      <Navbar />
-      <Link href="/travelList">여행 리스트</Link>
-      {session ? <NewTravel user={session} /> : ""}
-      {session ? <TravelList user={session} /> : ""}
-    </div>
-  );
+    return (
+        <div>
+            <Link href="/travelList">여행 리스트</Link>
+            {session ? <NewTravel user={session} /> : ''}
+            {session ? <TravelList user={session} /> : ''}
+        </div>
+    );
 }
