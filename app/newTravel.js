08aa3433fde691942f52f786a66f060387e42b3b
leaftha <vltpcks@gmail.com>
2024-05-17 15:20:59 +0900

    여행 추가 버튼 css 추가

--- app/newTravel.js
+++ app/newTravel.js
@@ -1,33 +1,31 @@
-"use client";
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
+import style from './newTravel.module.css';
 
 export default function NewTravel({ user }) {
-  let [clicked, setClicked] = useState(false);
-  function modal() {
-    setClicked(!clicked);
-  }
-  return (
-    <div>
-      <button onClick={modal}>추가</button>
-
-      {clicked ? (
+    let [clicked, setClicked] = useState(false);
+    function modal() {
+        setClicked(!clicked);
+    }
+    return (
         <div>
-          <form action="/api/post/newTravel" method="POST">
-            <input
-              name="email"
-              readOnly={true}
-              defaultValue={user.user.email}
-            />
-            <input name="title" placeholder="제목을 입력해 주세요" />
-            <input name="menber" type="number" min={1} />
-            <input name="money" type="number" min={1} />원
-            <button type="submit">작성</button>
-          </form>
+            <button className={style.btn} onClick={modal}>
+                추가
+            </button>
+
+            {clicked ? (
+                <div>
+                    <form action="/api/post/newTravel" method="POST">
+                        <input name="email" readOnly={true} defaultValue={user.user.email} />
+                        <input name="title" placeholder="제목을 입력해 주세요" />
+                        <input name="menber" type="number" min={1} />
+                        <input name="money" type="number" min={1} />원<button type="submit">작성</button>
+                    </form>
+                </div>
+            ) : (
+                ''
+            )}
         </div>
-      ) : (
-        ""
-      )}
-    </div>
-  );
+    );
 }
