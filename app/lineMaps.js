d71401b18c401825ef4fc59b36c0e8178ca106b7
SaChan <vltpcks@gmail.com>
2024-05-18 22:13:55 +0900

    맵 css 추가

--- app/lineMaps.js
+++ app/lineMaps.js
@@ -1,10 +1,11 @@
 "use client";
 import Maps from "./Maps";
 import { Wrapper } from "@googlemaps/react-wrapper";
+import style from "./lineMap.module.css";
 
 export default function LineMaps({ day }) {
   return (
-    <div>
+    <div className={style.map}>
       <Wrapper
         apiKey={process.env.NEXT_PUBLIC_API}
         libraries={["places", "marker"]}
