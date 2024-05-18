acf84becd8d76a6f71311d0d7b9b844d896c1405
SaChan <vltpcks@gmail.com>
2024-05-18 22:11:23 +0900

    여행 리스트 css 추가

--- app/page.js
+++ app/page.js
@@ -1,23 +1,22 @@
-import Link from 'next/link';
-import LoginBtn from './loginbtn';
-import { authOptions } from '@/pages/api/auth/[...nextauth].js';
-import { getServerSession } from 'next-auth';
-import NewTravel from './newTravel';
-import TravelList from './travelList';
-import style from './page.module.css';
+import Link from "next/link";
+import { authOptions } from "@/pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth";
+import NewTravel from "./newTravel";
+import TravelList from "./travelList";
+import style from "./page.module.css";
 
 export default async function Home() {
-    let session = await getServerSession(authOptions);
+  let session = await getServerSession(authOptions);
 
-    return (
-        <div className={style.body}>
-            <div className={style.btns}>
-                <Link className={style.link} href="/travelList">
-                    여행 리스트
-                </Link>
-                {session ? <NewTravel user={session} /> : ''}
-            </div>
-            {session ? <TravelList user={session} /> : ''}
-        </div>
-    );
+  return (
+    <div className={style.body}>
+      <div className={style.btns}>
+        <Link className={style.link} href="/travelList">
+          여행 리스트
+        </Link>
+        {session ? <NewTravel user={session} /> : ""}
+      </div>
+      {session ? <TravelList user={session} /> : ""}
+    </div>
+  );
 }
--- app/travelList.js
+++ app/travelList.js
@@ -1,6 +1,7 @@
 import { connectDB } from "@/util/database";
 import Link from "next/link";
 import LineMaps from "./lineMaps";
+import style from "./travelList.module.css";
 
 export default async function TravelList({ user }) {
   let db = (await connectDB).db("travel");
@@ -9,14 +10,16 @@ export default async function TravelList({ user }) {
     .find({ email: user.user.email })
     .toArray();
   return (
-    <div>
+    <div className={style.main}>
       {result.map((item, idx) => (
-        <div key={idx}>
-          <Link href={`/travel/${item._id}`}>{item.title}</Link>
-          <h1>Money : {item.money}</h1>
-          <h1>Menber : {item.menber}</h1>
+        <div className={style.item} key={idx}>
+          <Link className={style.title} href={`/travel/${item._id}`}>
+            {item.title}
+          </Link>
+          <h1 className={style.money}>Money : {item.money}</h1>
+          <h1 className={style.menber}>Menber : {item.menber}</h1>
           {item.days.length === 0 ? (
-            <h1>일정 입력이 필요</h1>
+            <h1 className={style.need}>일정 입력이 필요</h1>
           ) : (
             <LineMaps num={idx} day={result[idx].days} />
           )}
