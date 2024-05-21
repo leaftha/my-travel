9d7939e1dd844cc879f8546b21af6bac67a269d7
SaChan <vltpcks@gmail.com>
2024-05-21 19:33:09 +0900

    하루 추가 css 추가

--- app/travel/[id]/addDate.js
+++ app/travel/[id]/addDate.js
@@ -1,9 +1,21 @@
+import style from "./addDate.module.css";
+
 export default function AddDate({ id, last }) {
   return (
     <form action="/api/post/addDate" method="POST">
-      <input name="id" defaultValue={id} readOnly={true} />
-      <input name="last" defaultValue={last} readOnly={true} />
-      <button>추가</button>
+      <input
+        className={style.none}
+        name="id"
+        defaultValue={id}
+        readOnly={true}
+      />
+      <input
+        className={style.none}
+        name="last"
+        defaultValue={last}
+        readOnly={true}
+      />
+      <button className={style.btn}>하루 추가</button>
     </form>
   );
 }
