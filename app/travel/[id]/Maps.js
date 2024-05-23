36b290da08818a49cdbf0daf4be44d9853bcd6ca
leaftha <vltpcks@gmail.com>
2024-05-23 17:07:58 +0900

    하루 삭제 버튼 위치 변경

--- app/travel/[id]/Maps.js
+++ app/travel/[id]/Maps.js
@@ -1,232 +1,232 @@
-"use client";
-import { useState, useEffect, useRef } from "react";
-import useGoogle from "react-google-autocomplete/lib/usePlacesAutocompleteService";
-import ImgUploader from "./imgUploader";
-import { getStorage, ref, deleteObject } from "firebase/storage";
-import storage from "@/firebase/storage";
-import Img from "./img";
-import style from "./Map.module.css";
+'use client';
+import { useState, useEffect, useRef } from 'react';
+import useGoogle from 'react-google-autocomplete/lib/usePlacesAutocompleteService';
+import ImgUploader from './imgUploader';
+import { getStorage, ref, deleteObject } from 'firebase/storage';
+import storage from '@/firebase/storage';
+import Img from './img';
+import style from './Map.module.css';
 
 export default function Maps({ day, id }) {
-  const [map, setMap] = useState(null);
-  const [place, setPlace] = useState(day.placeId[0]);
-  const [coors, setCoors] = useState([...day.placeId]);
-  // const [markers, setMarkers] = useState([...day.placeId]);
-  const [name, setName] = useState(day.place[0]);
-  const [names, setNames] = useState([...day.place]);
-  const [content, setContent] = useState("");
-  const [contents, setContents] = useState([...day.content]);
-  const [imgList, setImgList] = useState([...day.daysImg]);
-  const { placePredictions, getPlacePredictions, isPlacePredictionsLoading } =
-    useGoogle({
-      apiKey: process.env.NEXT_PUBLIC_API,
+    const [map, setMap] = useState(null);
+    const [place, setPlace] = useState(day.placeId[0]);
+    const [coors, setCoors] = useState([...day.placeId]);
+    // const [markers, setMarkers] = useState([...day.placeId]);
+    const [name, setName] = useState(day.place[0]);
+    const [names, setNames] = useState([...day.place]);
+    const [content, setContent] = useState('');
+    const [contents, setContents] = useState([...day.content]);
+    const [imgList, setImgList] = useState([...day.daysImg]);
+    const { placePredictions, getPlacePredictions, isPlacePredictionsLoading } = useGoogle({
+        apiKey: process.env.NEXT_PUBLIC_API,
     });
 
-  const Mapref = useRef();
+    const Mapref = useRef();
 
-  useEffect(() => {
-    const showMap = async () => {
-      let arr = [];
-      const curPlace = await geocodePlaceId(place);
-      const newMap = new google.maps.Map(Mapref.current, {
-        center: {
-          lat: curPlace.geometry.location.lat(),
-          lng: curPlace.geometry.location.lng(),
-        },
-        zoom: 14,
-        mapId: `Maps-${day.day}`,
-      });
+    useEffect(() => {
+        const showMap = async () => {
+            let arr = [];
+            const curPlace = await geocodePlaceId(place);
+            const newMap = new google.maps.Map(Mapref.current, {
+                center: {
+                    lat: curPlace.geometry.location.lat(),
+                    lng: curPlace.geometry.location.lng(),
+                },
+                zoom: 14,
+                mapId: `Maps-${day.day}`,
+            });
 
-      // 선택한 곳 마커
-      const marker = new google.maps.marker.AdvancedMarkerElement({
-        map: newMap,
-        position: {
-          lat: curPlace.geometry.location.lat(),
-          lng: curPlace.geometry.location.lng(),
-        },
-      });
+            // 선택한 곳 마커
+            const marker = new google.maps.marker.AdvancedMarkerElement({
+                map: newMap,
+                position: {
+                    lat: curPlace.geometry.location.lat(),
+                    lng: curPlace.geometry.location.lng(),
+                },
+            });
 
-      // 하루동안의 마커 생성
+            // 하루동안의 마커 생성
 
-      for (let id of coors) {
-        const markerId = await geocodePlaceId(id);
+            for (let id of coors) {
+                const markerId = await geocodePlaceId(id);
 
-        let coor = {
-          lat: markerId.geometry.location.lat(),
-          lng: markerId.geometry.location.lng(),
+                let coor = {
+                    lat: markerId.geometry.location.lat(),
+                    lng: markerId.geometry.location.lng(),
+                };
+                arr.push(coor);
+                const marker = new google.maps.marker.AdvancedMarkerElement({
+                    map: newMap,
+                    position: coor,
+                });
+            }
+
+            // 마커 라인 그리기
+            if (arr.length > 1) {
+                const flightPath = new google.maps.Polyline({
+                    path: arr,
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 1.0,
+                    strokeWeight: 2,
+                    map: newMap,
+                });
+            }
+            setMap(newMap);
         };
-        arr.push(coor);
-        const marker = new google.maps.marker.AdvancedMarkerElement({
-          map: newMap,
-          position: coor,
-        });
-      }
+        showMap();
+    }, [place, coors, imgList]);
 
-      // 마커 라인 그리기
-      if (arr.length > 1) {
-        const flightPath = new google.maps.Polyline({
-          path: arr,
-          strokeColor: "#FF0000",
-          strokeOpacity: 1.0,
-          strokeWeight: 2,
-          map: newMap,
-        });
-      }
-      setMap(newMap);
-    };
-    showMap();
-  }, [place, coors, imgList]);
+    const deletContent = (idx) => {
+        setContents(contents.filter((item, index) => index != idx));
+        setCoors(coors.filter((item, index) => index != idx));
+        setNames(names.filter((item, index) => index != idx));
 
-  const deletContent = (idx) => {
-    setContents(contents.filter((item, index) => index != idx));
-    setCoors(coors.filter((item, index) => index != idx));
-    setNames(names.filter((item, index) => index != idx));
-
-    // firebase에서 이미지 삭제
-    for (let img of imgList[idx]) {
-      const desertRef = ref(storage, `images/${img}`);
-      deleteObject(desertRef)
-        .then(() => {
-          console.log("delete success");
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-
-    // 현재 이미지 배열 내의 이미지 삭제
-    const newImgList = [...imgList];
-    newImgList.splice(idx, 1);
-    setImgList(newImgList);
-  };
-
-  // 이미지 url 반환 받기
-  const getImg = (name, idx) => {
-    let newArr = [...imgList];
-    newArr[idx].push(name);
-    setImgList([...newArr]);
-  };
-
-  // 주소 받기
-  const geocodePlaceId = (placeId) => {
-    return new Promise((resolve, reject) => {
-      const geocoder = new google.maps.Geocoder();
-      geocoder.geocode({ placeId }, (results, status) => {
-        if (status === "OK" && results[0]) {
-          resolve(results[0]);
-        } else {
-          reject(status);
+        // firebase에서 이미지 삭제
+        for (let img of imgList[idx]) {
+            const desertRef = ref(storage, `images/${img}`);
+            deleteObject(desertRef)
+                .then(() => {
+                    console.log('delete success');
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
         }
-      });
-    });
-  };
-  return (
-    <div className={style.main}>
-      <div
-        ref={Mapref}
-        id="map"
-        style={{ width: "500px", height: "500px" }}
-      ></div>
-      <div className={style.lists}>
-        <div className={style.inputs}>
-          <h1 className={style.title}>To Did</h1>
-          {/* 주소 입력 창 */}
-          <label>주소 입력</label>
-          <input
-            onChange={(e) => {
-              getPlacePredictions({ input: e.target.value });
-            }}
-          />
-          {/* 주소 자동 완성  */}
-          {placePredictions.length != 0
-            ? placePredictions.map((item, idx) => (
-                <h1
-                  key={idx}
-                  onClick={(e) => {
-                    setPlace(item.place_id);
-                    setName(item.description);
-                  }}
-                >
-                  {item.description}
-                </h1>
-              ))
-            : ""}
-          {/* 했던 일 입력 창 */}
-          <label>내용 입력</label>
-          <input
-            className={style.todidInput}
-            value={content}
-            onChange={(e) => {
-              setContent(e.target.value);
-            }}
-          />
-          {/* 주소 and 했던일 추가 버튼 */}
-          <button
-            className={style.btn}
-            onClick={() => {
-              fetch("/api/post/addPlaceData", {
-                method: "POST",
-                body: JSON.stringify({
-                  id: id,
-                  day: day.day,
-                  content: content,
-                  placeId: place,
-                  name: name,
-                }),
-              });
-              if (names[0] === "") {
-                setCoors([place]);
-                setNames([name]);
-              } else {
-                setCoors([place, ...coors]);
-                setNames([name, ...names]);
-              }
-              setImgList([...imgList, []]);
-              setContents([content, ...contents]);
-              setContent("");
-            }}
-          >
-            입력
-          </button>
-        </div>
-        {/* 했던일 보여주기 */}
-        {contents.length === 0 ? (
-          <h1>했던일을 입력하세요</h1>
-        ) : (
-          contents.map((item, idx) => (
-            <div key={idx}>
-              <h1>
-                {item} - {names[idx]}
-              </h1>
-              {/* 이미지 업로드 기능 */}
-              <ImgUploader fuc={getImg} day={day} idx={idx} id={id} />
 
-              {/* 이미지 리스트 보여주기 */}
-              {imgList[idx].map((name, index) => (
-                <Img key={index} img={name} />
-              ))}
+        // 현재 이미지 배열 내의 이미지 삭제
+        const newImgList = [...imgList];
+        newImgList.splice(idx, 1);
+        setImgList(newImgList);
+    };
 
-              {/* 했던일 삭제 버튼 */}
-              <h1
-                onClick={() => {
-                  fetch("/api/post/deleteToDid", {
-                    method: "POST",
-                    body: JSON.stringify({
-                      id: id,
-                      day: day.day,
-                      contents: [...contents],
-                      idx: idx,
-                    }),
-                  });
-                  deletContent(idx);
-                }}
-              >
-                X
-              </h1>
+    // 이미지 url 반환 받기
+    const getImg = (name, idx) => {
+        let newArr = [...imgList];
+        newArr[idx].push(name);
+        setImgList([...newArr]);
+    };
+
+    // 주소 받기
+    const geocodePlaceId = (placeId) => {
+        return new Promise((resolve, reject) => {
+            const geocoder = new google.maps.Geocoder();
+            geocoder.geocode({ placeId }, (results, status) => {
+                if (status === 'OK' && results[0]) {
+                    resolve(results[0]);
+                } else {
+                    reject(status);
+                }
+            });
+        });
+    };
+    return (
+        <div className={style.main}>
+            <div ref={Mapref} id="map" style={{ width: '500px', height: '500px' }}></div>
+            <div className={style.lists}>
+                <form action="/api/post/deleteDate" method="POST">
+                    <input className={style.none} name="id" defaultValue={id} readOnly={true} />
+                    {/* <input className={style.none} name="idx" defaultValue={idx} readOnly={true} /> */}
+                    <button className={style.btn}>하루 삭제</button>
+                </form>
+                <div className={style.inputs}>
+                    <h1 className={style.title}>To Did</h1>
+                    {/* 주소 입력 창 */}
+                    <label>주소 입력</label>
+                    <input
+                        onChange={(e) => {
+                            getPlacePredictions({ input: e.target.value });
+                        }}
+                    />
+                    {/* 주소 자동 완성  */}
+                    {placePredictions.length != 0
+                        ? placePredictions.map((item, idx) => (
+                              <h1
+                                  key={idx}
+                                  onClick={(e) => {
+                                      setPlace(item.place_id);
+                                      setName(item.description);
+                                  }}
+                              >
+                                  {item.description}
+                              </h1>
+                          ))
+                        : ''}
+                    {/* 했던 일 입력 창 */}
+                    <label>내용 입력</label>
+                    <input
+                        className={style.todidInput}
+                        value={content}
+                        onChange={(e) => {
+                            setContent(e.target.value);
+                        }}
+                    />
+                    {/* 주소 and 했던일 추가 버튼 */}
+                    <button
+                        className={style.btn}
+                        onClick={() => {
+                            fetch('/api/post/addPlaceData', {
+                                method: 'POST',
+                                body: JSON.stringify({
+                                    id: id,
+                                    day: day.day,
+                                    content: content,
+                                    placeId: place,
+                                    name: name,
+                                }),
+                            });
+                            if (names[0] === '') {
+                                setCoors([place]);
+                                setNames([name]);
+                            } else {
+                                setCoors([place, ...coors]);
+                                setNames([name, ...names]);
+                            }
+                            setImgList([...imgList, []]);
+                            setContents([content, ...contents]);
+                            setContent('');
+                        }}
+                    >
+                        입력
+                    </button>
+                </div>
+                {/* 했던일 보여주기 */}
+                {contents.length === 0 ? (
+                    <h1>했던일을 입력하세요</h1>
+                ) : (
+                    contents.map((item, idx) => (
+                        <div key={idx}>
+                            <h1>
+                                {item} - {names[idx]}
+                            </h1>
+                            {/* 이미지 업로드 기능 */}
+                            <ImgUploader fuc={getImg} day={day} idx={idx} id={id} />
+
+                            {/* 이미지 리스트 보여주기 */}
+                            {imgList[idx].map((name, index) => (
+                                <Img key={index} img={name} />
+                            ))}
+
+                            {/* 했던일 삭제 버튼 */}
+                            <h1
+                                onClick={() => {
+                                    fetch('/api/post/deleteToDid', {
+                                        method: 'POST',
+                                        body: JSON.stringify({
+                                            id: id,
+                                            day: day.day,
+                                            contents: [...contents],
+                                            idx: idx,
+                                        }),
+                                    });
+                                    deletContent(idx);
+                                }}
+                            >
+                                X
+                            </h1>
+                        </div>
+                    ))
+                )}
             </div>
-          ))
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
--- app/travel/[id]/page.js
+++ app/travel/[id]/page.js
@@ -1,98 +1,74 @@
-import { ObjectId } from "mongodb";
-import { connectDB } from "@/util/database.js";
-import { authOptions } from "@/pages/api/auth/[...nextauth].js";
-import { getServerSession } from "next-auth";
+import { ObjectId } from 'mongodb';
+import { connectDB } from '@/util/database.js';
+import { authOptions } from '@/pages/api/auth/[...nextauth].js';
+import { getServerSession } from 'next-auth';
 
-import AddDate from "./addDate";
-import ThisDay from "./thisDay";
-import Link from "next/link";
-import Likse from "./likes";
-import ImgModal from "./imgModal";
-import style from "./page.module.css";
+import AddDate from './addDate';
+import ThisDay from './thisDay';
+import Link from 'next/link';
+import Likse from './likes';
+import ImgModal from './imgModal';
+import style from './page.module.css';
 
 export default async function Detail(props) {
-  let session = await getServerSession(authOptions);
+    let session = await getServerSession(authOptions);
 
-  // 로그인 예외 처리
-  if (session === null) {
-    return NotAuth();
-  }
-  // 게시물 db
-  let db = (await connectDB).db("travel");
-  let result = await db
-    .collection("travelPost")
-    .findOne({ _id: new ObjectId(props.params.id) });
-
-  // 유저 db
-  let user = await db
-    .collection("user_id")
-    .findOne({ email: session.user.email });
-  user._id = user._id.toString();
-  let last_day = 0;
-  if (result.days.length != 0) {
-    last_day = result.days.length;
-  }
-
-  let Imgs = [];
-  for (let day of result.days) {
-    if (day.daysImg.length > 0) {
-      let newArray = day.daysImg.reduce(function (prev, next) {
-        return prev.concat(next);
-      });
-      Imgs.push(...newArray);
+    // 로그인 예외 처리
+    if (session === null) {
+        return NotAuth();
     }
-  }
+    // 게시물 db
+    let db = (await connectDB).db('travel');
+    let result = await db.collection('travelPost').findOne({ _id: new ObjectId(props.params.id) });
 
-  return (
-    <div className={style.main}>
-      <div className={style.menu}>
-        <form action="/api/post/deleteTravel" method="POST">
-          <input
-            className={style.none}
-            name="id"
-            defaultValue={props.params.id}
-            readOnly={true}
-          />
-          <button className={style.btn}>여행 삭제</button>
-        </form>
-      </div>
-      <div className={style.content}>
-        <h1 className={style.title}>{result.title}</h1>
-        <div className={style.btns}>
-          <Likse
-            isprivate={result.private}
-            like={result.like}
-            id={props.params.id}
-            email={session.user.email}
-          />
-          <AddDate last={last_day} id={props.params.id} />
-          <ImgModal img={Imgs} />
-        </div>
-        {result.days.length === 0 ? (
-          <h1>스케줄을 추가 하세요</h1>
-        ) : (
-          result.days.map((item, idx) => (
-            <div className={style.body}>
-              <form action="/api/post/deleteDate" method="POST">
-                <input
-                  className={style.none}
-                  name="id"
-                  defaultValue={props.params.id}
-                  readOnly={true}
-                />
-                <input
-                  className={style.none}
-                  name="idx"
-                  defaultValue={idx}
-                  readOnly={true}
-                />
-                <button className={style.btn}>하루 삭제</button>
-              </form>
-              <ThisDay key={idx} id={props.params.id} day={item} />
+    // 유저 db
+    let user = await db.collection('user_id').findOne({ email: session.user.email });
+    user._id = user._id.toString();
+    let last_day = 0;
+    if (result.days.length != 0) {
+        last_day = result.days.length;
+    }
+
+    let Imgs = [];
+    for (let day of result.days) {
+        if (day.daysImg.length > 0) {
+            let newArray = day.daysImg.reduce(function (prev, next) {
+                return prev.concat(next);
+            });
+            Imgs.push(...newArray);
+        }
+    }
+
+    return (
+        <div className={style.main}>
+            <div className={style.menu}>
+                <form action="/api/post/deleteTravel" method="POST">
+                    <input className={style.none} name="id" defaultValue={props.params.id} readOnly={true} />
+                    <button className={style.btn}>여행 삭제</button>
+                </form>
             </div>
-          ))
-        )}
-      </div>
-    </div>
-  );
+            <div className={style.content}>
+                <h1 className={style.title}>{result.title}</h1>
+                <div className={style.btns}>
+                    <Likse
+                        isprivate={result.private}
+                        like={result.like}
+                        id={props.params.id}
+                        email={session.user.email}
+                    />
+                    <AddDate last={last_day} id={props.params.id} />
+                    <ImgModal img={Imgs} />
+                </div>
+                {result.days.length === 0 ? (
+                    <h1>스케줄을 추가 하세요</h1>
+                ) : (
+                    result.days.map((item, idx) => (
+                        <div className={style.body}>
+                            <ThisDay key={idx} id={props.params.id} day={item} />
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
+    );
 }
