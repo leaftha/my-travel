ac4ca9c983590a41cc3d8adf8d815e2aa28a4a6b
SaChan <vltpcks@gmail.com>
2024-05-29 21:10:08 +0900

    여행 날짜 보여주는 방식을 페이지 방식으로 변경

---
+++ app/travel/[id]/dayList.js
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState } from "react";
+import ThisDay from "./thisDay";
+import style from "./dayList.module.css";
+
+export default function DayList({ days, id, item }) {
+  const [currentDay, setCurrentDay] = useState(0);
+  let last_day = 0;
+  if (days.days.length != 0) {
+    last_day = days.days.length;
+  }
+  return (
+    <div className={style.main}>
+      <div className={style.pages}>
+        {days.days.length &&
+          days.days.map((item, idx) => (
+            <button
+              onClick={() => {
+                setCurrentDay(idx);
+              }}
+              key={idx}
+            >
+              {idx + 1}일
+            </button>
+          ))}
+        <form action="/api/post/addDate" method="POST">
+          <input
+            className={style.none}
+            name="id"
+            defaultValue={id}
+            readOnly={true}
+          />
+          <input
+            className={style.none}
+            name="last"
+            defaultValue={last_day}
+            readOnly={true}
+          />
+          <button>하루 추가</button>
+        </form>
+      </div>
+      {days.days.length === 0 ? (
+        <h1>스케줄을 추가 하세요</h1>
+      ) : (
+        <div>
+          <ThisDay key={currentDay} id={id} day={days.days[currentDay]} />
+        </div>
+      )}
+    </div>
+  );
+}
