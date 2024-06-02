6366ba36aba6285679ec967389db91bdf7a5554a
SaChan <vltpcks@gmail.com>
2024-06-02 13:53:04 +0900

    하루 지출 부분 css 추가

--- app/travel/[id]/thisDay.js
+++ app/travel/[id]/thisDay.js
@@ -10,29 +10,32 @@ export default function ThisDay({ day, id }) {
   const [money, setMoney] = useState(day.money);
   return (
     <div className={style.main}>
-      <label>지출</label>
-      <input
-        type="number"
-        onChange={(e) => {
-          setMoney(e.target.value);
-        }}
-      />
-      <button
-        className={style.btn}
-        onClick={() => {
-          fetch("/api/post/addDateMoney", {
-            method: "POST",
-            body: JSON.stringify({
-              id: id,
-              day: day.day,
-              money: money,
-            }),
-          });
-        }}
-      >
-        입력
-      </button>
-      <h1>{money} 원</h1>
+      <div className={style.moneybody}>
+        <label className={style.title}>지출 : </label>
+        <input
+          className={style.inputbox}
+          type="number"
+          onChange={(e) => {
+            setMoney(e.target.value);
+          }}
+        />
+        <button
+          className={style.btn}
+          onClick={() => {
+            fetch("/api/post/addDateMoney", {
+              method: "POST",
+              body: JSON.stringify({
+                id: id,
+                day: day.day,
+                money: money,
+              }),
+            });
+          }}
+        >
+          입력
+        </button>
+        <h1 className={style.money}>{money} 원</h1>
+      </div>
 
       <Wrapper
         apiKey={process.env.NEXT_PUBLIC_API}
