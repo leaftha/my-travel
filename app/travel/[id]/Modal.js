050598babe76be0c7cfd1635896c3b5952fac0ba
SaChan <vltpcks@gmail.com>
2024-06-08 18:11:38 +0900

    이미지 모달 첫 이미지 마지막 이미지 표시

--- app/travel/[id]/Modal.js
+++ app/travel/[id]/Modal.js
@@ -1,15 +1,15 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import Img from "./img";
 import style from "./Modal.module.css";
 
 export default function Modal({ img, setModal }) {
-  const [current ,setCurrent] = useState(0)
+  const [current, setCurrent] = useState(0);
 
-  let imgWidth = window.innerWidth / 2 
-  let imgHeight = window.innerHeight / 2
-  console.log(current)
+  let imgWidth = window.innerWidth / 2;
+  let imgHeight = window.innerHeight / 2;
+  console.log(current);
   return (
     <div className={style.main}>
       <h1
@@ -21,24 +21,33 @@ export default function Modal({ img, setModal }) {
       >
         X
       </h1>
-      <h1 className={style.prev} onClick={() => {
-        if(current != 0) {
-          setCurrent(current-1)
-        }
-      }}>{'<'}</h1>
+      <h1
+        className={`${style.prev}  ${current === 0 && style.last}`}
+        onClick={() => {
+          if (current != 0) {
+            setCurrent(current - 1);
+          }
+        }}
+      >
+        {"<"}
+      </h1>
       <div className={style.imgs}>
         {img.map((name, idx) => (
-         <div className={idx != current && style.none}>
-           <Img key={idx} width={imgWidth} height={imgHeight} img={name} />
-         </div>
+          <div className={idx != current && style.none}>
+            <Img key={idx} width={imgWidth} height={imgHeight} img={name} />
+          </div>
         ))}
       </div>
-      <h1 className={style.next} onClick={()=> {
-        if(current != img.length-1) {
-          setCurrent(current+1)
-        }
-      }}>{'>'}</h1>
-
+      <h1
+        className={`${style.next}  ${current === img.length - 1 && style.last}`}
+        onClick={() => {
+          if (current != img.length - 1) {
+            setCurrent(current + 1);
+          }
+        }}
+      >
+        {">"}
+      </h1>
     </div>
   );
 }
