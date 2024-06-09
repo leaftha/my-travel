82ad7b0c23bdd3e1db0e263c01343bb91bcbd4a5
SaChan <vltpcks@gmail.com>
2024-06-09 16:52:03 +0900

    공개된 여행 리스트 css 추가

--- app/travelList/lineMaps.js
+++ app/travelList/lineMaps.js
@@ -2,9 +2,11 @@
 import Maps from "./Maps";
 import { Wrapper } from "@googlemaps/react-wrapper";
 
+import style from "./lineMap.module.css";
+
 export default function LineMaps({ num, day }) {
   return (
-    <div>
+    <div className={style.map}>
       <Wrapper
         apiKey={process.env.NEXT_PUBLIC_API}
         libraries={["places", "marker"]}
--- app/travelList/list.js
+++ app/travelList/list.js
@@ -1,22 +1,33 @@
-import { connectDB } from '@/util/database';
-import Link from 'next/link';
-import LineMaps from './lineMaps';
+import { connectDB } from "@/util/database";
+import Link from "next/link";
+import LineMaps from "./lineMaps";
+
+import style from "./list.module.css";
 
 export default async function List() {
-    let db = (await connectDB).db('travel');
+  let db = (await connectDB).db("travel");
 
-    // 공개로 설정한 여행들 검색 ==> 추후 좋아요 순으로 정렬 기능 예정
-    let result = await db.collection('travelPost').find({ private: true }).toArray();
-    return (
-        <div>
-            {result.map((item, idx) => (
-                <div key={idx}>
-                    <Link href={`/travelDetail/${item._id}`}>{item.title}</Link>
-                    <h1>Money : {item.money}</h1>
-                    <h1>Menber : {item.menber}</h1>
-                    {item.days.length === 0 ? <h1></h1> : <LineMaps num={idx} day={result[idx].days} />}
-                </div>
-            ))}
+  // 공개로 설정한 여행들 검색 ==> 추후 좋아요 순으로 정렬 기능 예정
+  let result = await db
+    .collection("travelPost")
+    .find({ private: true })
+    .toArray();
+  return (
+    <div className={style.main}>
+      {result.map((item, idx) => (
+        <div className={style.item} key={idx}>
+          <Link className={style.title} href={`/travelDetail/${item._id}`}>
+            {item.title}
+          </Link>
+          <h1 className={style.money}>Money : {item.money}</h1>
+          <h1 className={style.menber}>Menber : {item.menber}</h1>
+          {item.days.length === 0 ? (
+            <h1>아직 입력 중</h1>
+          ) : (
+            <LineMaps num={idx} day={result[idx].days} />
+          )}
         </div>
-    );
+      ))}
+    </div>
+  );
 }
