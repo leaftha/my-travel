f2c7e55ef402d0c4ad225ebcd16cb88738d92e41
leaftha <vltpcks@gmail.com>
2024-06-14 15:55:36 +0900

    메인 페이지 원 자릿수 표시

--- app/travelList.js
+++ app/travelList.js
@@ -18,8 +18,28 @@ export default async function TravelList({ user }) {
       for(let content of day.days) {
         count += content.money
       }
-      money.push(count)
+
+      let changeMoney = [];
+      let newMoney = `${count}`;
+      let Mcount = 0;
+      for (let i = newMoney.length - 1; i >= 0; i--) {
+        changeMoney.unshift(newMoney[i]);
+        if (Mcount === 2) {
+          changeMoney.unshift(",");
+          Mcount = 0;
+        } else {
+          Mcount++;
+        }
+      }
+      if (changeMoney[0] === ",") {
+        changeMoney.shift();
+      }
+      changeMoney.join("");
+      money.push(changeMoney)
     }
+
+  
+
   return (
     <div className={style.main}>
       {result.map((item, idx) => (
@@ -27,7 +47,7 @@ export default async function TravelList({ user }) {
           <Link className={style.title} href={`/travel/${item._id}`}>
             {item.title}
           </Link>
-          <h1 className={style.money}>총 금액 : {money[idx]}</h1>
+          <h1 className={style.money}>총 금액 : {money[idx]}원</h1>
           <h1 className={style.menber}>인원수 : {item.menber}</h1>
           {item.days.length === 0 ? (
             <h1 className={style.need}>일정 입력이 필요</h1>
