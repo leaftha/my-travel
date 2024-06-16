f7aa7d088be3c36627db0465b0d56f822d52a6de
SaChan <vltpcks@gmail.com>
2024-06-16 22:34:17 +0900

    좋아요한 게시물 css 추가

--- app/mypage/likeTravel.js
+++ app/mypage/likeTravel.js
@@ -1,15 +1,26 @@
+"use client";
+
 import Link from "next/link";
+
+import style from "./likeTravel.module.css";
 
 export default function LikeTravel({ likes }) {
   return (
-    <>
+    <div className={style.main}>
       {likes.map((travel, idx) => (
-        <div key={idx}>
-          <h1>제목 : {travel.title}</h1>
-          <h1>총 금액 : {travel.money}</h1>
-          <h1>인원 수 : {travel.menber}</h1>
-        </div>
+        <Link
+          key={idx}
+          className={style.link}
+          href={`/travelDetail/${travel._id}`}
+        >
+          <div className={style.item}>
+            <h1>제목 : {travel.title}</h1>
+            <h1>총 금액 : {travel.money}</h1>
+            <h1>인원 수 : {travel.menber}</h1>
+            <h1>좋아요 : {travel.like}</h1>
+          </div>
+        </Link>
       ))}
-    </>
+    </div>
   );
 }
--- app/mypage/page.js
+++ app/mypage/page.js
@@ -35,7 +35,7 @@ export default async function Home() {
           </div>
           <UserDelete email={user.email} />
         </div>
-        <div>
+        <div className={style.likes}>
           <LikeTravel likes={likes} />
         </div>
       </div>
