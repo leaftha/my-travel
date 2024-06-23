585bf69aedbe25d7e09c553dfa9a00c153b3f73d
SaChan <vltpcks@gmail.com>
2024-06-23 16:30:10 +0900

    돈 자릿 수 표시

--- app/travelList/page.js
+++ app/travelList/page.js
@@ -11,6 +11,33 @@ export default async function Detail() {
     .find({ private: true })
     .toArray();
 
+  for (let day of result) {
+    let count = 0;
+    day._id = day._id.toString();
+
+    for (let content of day.days) {
+      count += content.money;
+    }
+
+    let changeMoney = [];
+    let newMoney = `${count}`;
+    let Mcount = 0;
+    for (let i = newMoney.length - 1; i >= 0; i--) {
+      changeMoney.unshift(newMoney[i]);
+      if (Mcount === 2) {
+        changeMoney.unshift(",");
+        Mcount = 0;
+      } else {
+        Mcount++;
+      }
+    }
+    if (changeMoney[0] === ",") {
+      changeMoney.shift();
+    }
+    changeMoney.join("");
+    day.money = changeMoney;
+  }
+
   return (
     <div>
       <List travel={result} />
