7ffecf31f520f8a164492c1c9a82a50b90f5a2f1
SaChan <vltpcks@gmail.com>
2024-06-23 17:21:11 +0900

    select 박스 css 추가

--- app/travelList/selectTravel.js
+++ app/travelList/selectTravel.js
@@ -1,11 +1,15 @@
+"use client";
+
+import style from "./selectTravel.module.css";
+
 export default function SelectTravel({ setSort }) {
   const handler = (e) => {
     setSort(e.target.value);
   };
 
   return (
-    <div>
-      <select onChange={handler}>
+    <div className={style.main}>
+      <select className={style.select} onChange={handler}>
         <option key="fast" value="fast">
           최신순
         </option>
