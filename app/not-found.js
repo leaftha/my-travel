844ec9a72e20f07c8aa793dc108c1833a46fa8d4
SaChan <vltpcks@gmail.com>
2024-06-30 13:17:20 +0900

    없는 페이지 예외처리 css추가

--- app/not-found.js
+++ app/not-found.js
@@ -1,6 +1,9 @@
+import style from "./not-found.module.css";
+
 export default function NotFound() {
   return (
-    <div>
+    <div className={style.main}>
+      <h1>404</h1>
       <h1>없는 페이지입니다.</h1>
     </div>
   );
