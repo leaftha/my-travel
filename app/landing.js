6a82c3179fe5a053995042788d16418d9b70d546
leaftha <vltpcks@gmail.com>
2025-01-24 10:01:05 +0900

    렌딩 페이지 디자인 추가

--- app/landing.js
+++ app/landing.js
@@ -1,28 +1,38 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import style from "./lading.module.css";
+import { useEffect, useState } from 'react';
+import style from './lading.module.css';
 
 const Landing = () => {
-  const [currentimg, setCurrentImg] = useState(0);
+    const [currentImg, setCurrentImg] = useState(0);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setCurrentImg(currentimg + 1);
-    }, 1000);
-  }, []);
-  return (
-    <div className={style.imgSlider}>
-      {Array(5).map((iem, idx) => (
-        // <img
-        //   src={`./img/main${idx + 1}.jpg`}
-        //   key={`img${idx}`}
-        //   //   style={currentimg === idx ? { opacity: 1 } : { opacity: 0 }}
-        // />
-        <h1>{idx}</h1>
-      ))}
-    </div>
-  );
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCurrentImg((prev) => (prev + 1) % 5);
+        }, 30000);
+
+        return () => clearTimeout(timer);
+    }, [currentImg]);
+
+    return (
+        <div className={style.imgSlider}>
+            <div className={style.des}>
+                <h1>MY-Travel</h1>
+                <p>자신의 여행을 다른 사람들에게 공유하세요!</p>
+            </div>
+            {Array.from({ length: 5 }).map((_, idx) => (
+                <img
+                    src={`/img/main${idx + 1}.jpg`}
+                    key={`img${idx}`}
+                    className={style.image}
+                    style={{
+                        opacity: currentImg === idx ? 1 : 0,
+                    }}
+                />
+            ))}
+            <div className={style.imgCover}></div>
+        </div>
+    );
 };
 
 export default Landing;
--- app/navbar.js
+++ app/navbar.js
@@ -1,38 +1,38 @@
-import { authOptions } from "@/pages/api/auth/[...nextauth].js";
-import { getServerSession } from "next-auth";
-import LoginBtn from "./loginbtn";
-import Link from "next/link";
+import { authOptions } from '@/pages/api/auth/[...nextauth].js';
+import { getServerSession } from 'next-auth';
+import LoginBtn from './loginbtn';
+import Link from 'next/link';
 
-import style from "./navbar.module.css";
+import style from './navbar.module.css';
 
 export default async function Navbar() {
-  let session = await getServerSession(authOptions);
+    let session = await getServerSession(authOptions);
 
-  return (
-    <nav className={style.navbar}>
-      <div>
-        <Link className={style.title} href="/">
-          Trip
-        </Link>
-        <Link className={style.link} href="/travelList">
-          여행 리스트
-        </Link>
-      </div>
-      <div className={style.logined}>
-        {session && (
-          <Link className={style.setting} href={`/mypage`}>
-            My Page
-          </Link>
-        )}
-        <LoginBtn session={session} />
-        {session ? (
-          ""
-        ) : (
-          <Link className={style.register} href="register">
-            회원가입
-          </Link>
-        )}
-      </div>
-    </nav>
-  );
+    return (
+        <nav className={style.navbar}>
+            <div className={style.titleContainer}>
+                <Link className={style.title} href="/">
+                    Trip
+                </Link>
+                <Link className={style.link} href="/travelList">
+                    여행 리스트
+                </Link>
+            </div>
+            <div className={style.logined}>
+                {session && (
+                    <Link className={style.setting} href={`/mypage`}>
+                        My Page
+                    </Link>
+                )}
+                <LoginBtn session={session} />
+                {session ? (
+                    ''
+                ) : (
+                    <Link className={style.register} href="register">
+                        회원가입
+                    </Link>
+                )}
+            </div>
+        </nav>
+    );
 }
